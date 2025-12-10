8f0b6eae63dff27fa623a3fbe868acfffbb26ee2
Shiki <3238267385@qq.com>
2025-12-10 15:08:29 +0800

    Initialize repo

---
+++ tests/test.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+def test_qt():
+    # 创建应用实例
+    app = QApplication([])
+
+    # 创建主窗口
+    window = QWidget()
+    window.setWindowTitle("我的第一个 PyQt 程序")
+    window.setGeometry(100, 100, 800, 600)  # (x, y, width, height)
+
+    # 显示窗口
+    window.show()
+
+    # 运行应用
+    app.exec_()
+
+
+if __name__ == "__main__":
+    test_qt()
